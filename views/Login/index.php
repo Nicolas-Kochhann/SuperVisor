040d7c4734931f2d08e7eeba3418a3115bd22c4e
Mathias Scherer <mathischerer@gmail.com>
2025-10-21 14:11:18 -0300

    Tela de login

---
+++ views/Login/index.php
@@ -0,0 +1,42 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SuperVisor</title>
+    <link rel="stylesheet" href="../../styles/style.css">
+    <link rel="icon" href="../../resources/images/favicon.ico">
+</head>
+<body>
+    <div class="container">
+        
+        <header class="cabecalho">
+            <img src="../../resources/images/logo.png" alt="Logo SuperVisor" class="logo-cabecalho">
+        </header>
+
+        <main class="container-formulario">
+            <form method="POST" action="index.php" enctype="multipart/form-data" class="formulario-grande">
+                <h1 class="titulo-formulario-grande">Login</h1>
+                <label for="email" class="label-form-grande obrigatorio">E-mail</label>
+                <input type="email" name="email" id="email" class="input-form-grande" required>
+                <label for="senha" class="label-form-grande obrigatorio">Senha</label>
+                <div class="password-container">
+                    <input class="input-form-grande" type="password" name="senha" id="senha" required>
+                    <img class="show-password" id="show-password" src="../../resources/images/eye.svg" alt="show passwd">
+                </div>
+                <p class="texto-obrigatorio">* indica algo obrigatório</p>
+                <span style="color: red"><?php echo $emailErro ?></span>
+                <span style="color: red"><?php echo $senhaErro ?></span>
+                <button id="submit" name="submit" class="botao-strong">Acessar</button>
+                <a href="" class="link-formulario">Cadastro</a>
+            </form>
+        </main>
+    </div>
+    <script src="../../scripts/mostraSenha.js"></script>
+</body>
+</html>
