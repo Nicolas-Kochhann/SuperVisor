a2a2658e8d4a0fa8cc043fa02fde94337eb617a8
Mathias Scherer <mathischerer@gmail.com>
2025-10-26 21:35:30 -0300

    me contrata itau

--- src/models/Usuario.php
+++ src/models/Usuario.php
@@ -5,6 +5,13 @@ namespace Src\models;
 use Src\database\MySQL;
 use RuntimeException;
 
+if (!function_exists('str_ends_with')) {
+    function str_ends_with(string $haystack, string $needle): bool
+    {
+        $needle_len = strlen($needle);
+        return ($needle_len === 0 || 0 === substr_compare($haystack, $needle, -$needle_len));
+    }
+}
 
 class Usuario{
     private int $idUsuario;
