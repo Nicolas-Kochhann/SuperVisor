4edc905c168f4c31f47a01fb7bdefc9afdf91ab3
Luiz <luizhenriquempmngk@gmail.com>
2025-11-04 14:07:22 -0300

    função atualizar atualizada

--- views/Perfil/index.php
+++ views/Perfil/index.php
@@ -1,3 +1,21 @@
+<?php 
+
+session_start();
+
+if(!isset($_SESSION["idUsuario"])){
+    $_SESSION["error"] = "É necessário entrar na sua conta antes disso.";
+    header("location: ../Login/");
+    exit();
+}
+
+require __DIR__."/../../vendor/autoload.php";
+use Src\models\Usuario;
+
+if (isset($_SESSION['idUsuario'])){
+    $user = Usuario::acharUsuario($_SESSION['idUsuario']);
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
