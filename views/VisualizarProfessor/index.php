b2ff45c836b8b29b49c6374b599214653362cc41
Mathias <mathischerer@gmail.com>
2025-11-04 14:01:56 -0300

    HELP MEEEE

--- views/VisualizarProfessor/index.php
+++ views/VisualizarProfessor/index.php
@@ -1,11 +1,26 @@
+<?php
+
+session_start();
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>SuperVisor</title>
+    <link rel="stylesheet" href="../../styles/style.css">
+    <link rel="icon" href="../../resources/images/favicon.ico">
 </head>
 <body>
+    <div class="container">
+        
+        <header class="cabecalho">
+            <img src="../../resources/images/logo.png" alt="Logo SuperVisor" class="logo-cabecalho">
+            <a class="cabecalho-link-botao" href="../Logout/">Sair</a>
+        </header>
     
+    </div>
 </body>
 </html>
