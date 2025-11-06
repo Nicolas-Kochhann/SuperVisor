10c477d83bc28c12ea1dd6431aa16c9a17d874b1
Mathias <mathischerer@gmail.com>
2025-11-06 16:48:15 -0300

    who you jivin' with that cosmik debris

--- views/CriarSolicitacao/index.php
+++ views/CriarSolicitacao/index.php
@@ -24,17 +24,31 @@
                 <p>Tudo que você preencher aqui será visível para os professores selecionados.</p>
 
                 <label for="empresa">Nome da Empresa</label>
-                <input type="text" name="empresa" id="empresa">
+                <input type="text" name="empresa" id="empresa" required>
                 
                 <label for="area-atuacao">Área de Atuação</label>
-                <input type="text" name="area-atuacao" id="area-atuacao">
+                <input type="text" name="area-atuacao" id="area-atuacao" required>
 
                 <label for="tipo-estagio">Tipo de Estágio</label>
-                <select name="tipo-estagio" id="tipo-estagio">
+                <select name="tipo-estagio" id="tipo-estagio" required>
                     <option value="obrigatorio">Obrigatório</option>
                     <option value="nao-obrigatorio">Não Obrigatório</option>
                     <option value="nao-sei">Não Sei</option>
                 </select>
+
+                <span class="multi-input-box">
+                    <label for="carga-horaria">Carga Horária Semanal</label>
+                    <input type="number" name="carga-horaria" id="carga-horaria">
+
+                    <label for="turno">Turno</label>
+                    <select name="turno" id="turno">
+                        <option value="manha">Manhã</option>
+                        <option value=""></option>
+                    </select>
+                </span>
+
+                <label for="obs">Obs.</label>
+                <textarea name="obs" id="obs">-</textarea>
             </form>
         </main>
 
