b7ecf62c483de9bb4b4899602d97e22a46cc3a3a
Mathias <mathischerer@gmail.com>
2025-11-13 17:09:54 -0300

    farm de commit²¹

--- views/nav.php
+++ views/nav.php
@@ -1,10 +1,10 @@
 <aside class="menu" id="menu">
     <?php
     if($_SESSION["tipo"]=="professor"){
-    echo"<a href='../ListarSolicitacoes/index.php'>Ver minhas solicitações</a>";
+    echo"<a href='../ListarSolicitacao/'>Ver minhas solicitações</a>";
     }else{
-        echo "<a href='../CriarSolicitacao/index.php' >Nova Solicitacao</a>
-        <a href='../TelaInicial/index.php' >Tela Inicial</a>";
+        echo "<a href='../CriarSolicitacao/' >Nova Solicitacao</a>
+        <a href='../TelaInicial/' >Tela Inicial</a>";
     }
     
     ?>
