d14bdae0d901e8d53a7ca9d82c3650ac21602f57
Nícolas <kochhann.nicolass.soldi@gmail.com>
2025-11-25 14:40:01 -0300

    "Are you experienced"

---
+++ scripts/gerarSenhaAleatoria.js
@@ -0,0 +1,11 @@
+function gerarSenhaAleatoria(){
+    const alfabeto = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    let senha = "SV0";
+
+    for(let i = 0; i < 5; i++){
+        senha += alfabeto.charAt(Math.floor(Math.random() * alfabeto.length));
+    }
+
+    const senhaInput = document.getElementById('senha');
+    senhaInput.value = senha;
+}
