99df54956dc9226e17bfaeee81abbb877a56c34a
Mathias Scherer <mathischerer@gmail.com>
2025-11-27 20:07:35 -0300

    morte certa

--- scripts/gerarSenhaAleatoria.js
+++ scripts/gerarSenhaAleatoria.js
@@ -1,12 +1,16 @@
-function gerarSenhaAleatoria(){
-    const alfabeto = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    let senha = "SV0";
+const changeEvent = new Event("input", { bubbles: true });
 
-    for(let i = 0; i < 5; i++){
-        senha += alfabeto.charAt(Math.floor(Math.random() * alfabeto.length));
-    }
+function gerarSenhaAleatoria() {
+  const alfabeto =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let senha = "SV0";
 
-    const senhaInput = document.getElementById('senha');
-    senhaInput.value = senha;
+  for (let i = 0; i < 5; i++) {
+    senha += alfabeto.charAt(Math.floor(Math.random() * alfabeto.length));
+  }
+
+  const senhaInput = document.getElementById("senha");
+  senhaInput.value = senha;
+
+  passwordField.dispatchEvent(changeEvent);
 }
-
